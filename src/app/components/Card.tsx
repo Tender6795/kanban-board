8b60faedd7e69c3d91e97eba2a3864b595ca2210
Muchnik Illia <muchnic.illia.js@gmail.com>
2024-07-29 13:32:09 +0300

    Card in progress

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Card = () => (
+  <div className="w-[300px] h-[185px] p-5 rounded-lg  bg-white flex items-start gap-2">
+    <div className="w-[56px] h-[16px] p-[2px_10px] bg-black rounded-[10px]"></div>
+    <div className="w-[55px] h-[16px] px-[10px] flex items-center  border-[0.5px] border-[#D1D5DB] rounded-[10px] text-[#64748B] font-inter text-[10px] font-normal leading-[16px] tracking-[-0.1px] text-left">
+      Réseau
+    </div>
+  </div>
+);
+
+export default Card;
